8b475c4cdfff51fcc3ddba684c8ce6de79de76b7
Hiten Nath <2024.hiten.nath@ves.ac.in>
2026-06-27 17:25:23 +0530

    delete useless comments

--- src/AttendanceTrackerApplication.java
+++ src/AttendanceTrackerApplication.java
@@ -23,7 +23,7 @@ public class AttendanceTrackerApplication {
 
         boolean keepRunning = true;
 
-        do{
+        do {
             System.out.println("Menu");
             System.out.println("1. Add Subject");
             System.out.println("2. View Subject");
@@ -34,10 +34,10 @@ public class AttendanceTrackerApplication {
             System.out.println("7. Exit");
 
             int choice = sc.nextInt();
-             //As subject is required for multiple cases we cant declare it inside any case.
+            //As subject is required for multiple cases we cant declare it inside any case.
             //As this will reduce the scope of our object reference.
 
-            switch (choice){
+            switch (choice) {
                 case 1:
                     System.out.println("Enter Subject Name");
                     String subjectName = sc.next();
@@ -54,10 +54,10 @@ public class AttendanceTrackerApplication {
                     System.out.println("Enter Subject index");
                     int i = sc.nextInt();
                     Subject chooseSubject = null;
-                    if (i>=0 && i<student.getSubjectList().size()) {
+                    if (i >= 0 && i < student.getSubjectList().size()) {
                         chooseSubject = student.getSubjectList().get(i);
 
-                    }else {
+                    } else {
                         System.out.println("Please enter correct index.");
                     }
 
@@ -66,7 +66,7 @@ public class AttendanceTrackerApplication {
                     DayOfWeek dayOfWeek;
                     try {
                         dayOfWeek = DayOfWeek.valueOf(day);
-                    }catch (IllegalArgumentException e){
+                    } catch (IllegalArgumentException e) {
                         System.out.println("Invalid day. Please try again");
                         break;
                     }
@@ -77,8 +77,7 @@ public class AttendanceTrackerApplication {
                     LocalTime time;
                     try {
                         time = LocalTime.parse(timeInput, timeFormatter);
-                        }
-                    catch (DateTimeParseException e){
+                    } catch (DateTimeParseException e) {
                         System.out.println("Invalid time format. Please use hours-minutes.");
                         break;
                     }
@@ -92,14 +91,14 @@ public class AttendanceTrackerApplication {
                     break;
 
                 case 5:
-                    if(student.getSubjectList().size() == 0){
+                    if (student.getSubjectList().size() == 0) {
                         System.out.println("Please add a subject first.");
                         break;
-                    }else{
+                    } else {
                         student.showSubjects();
                         System.out.println("Enter Subject index");
                         int n = sc.nextInt();
-                        if(n>=0 && n<student.getSubjectList().size()){
+                        if (n >= 0 && n < student.getSubjectList().size()) {
                             Subject choosenSubject = student.getSubjectList().get(n);
                             System.out.print("Enter a date (yyyy-MM-dd): ");
                             String inputDate = sc.next(); //Input is taken in orm of String
@@ -115,23 +114,10 @@ public class AttendanceTrackerApplication {
                             boolean present = sc.nextBoolean();
                             AttendanceStatus attendanceStatus = new AttendanceStatus(date, choosenSubject, present);
                             student.addAttendanceRecord(attendanceStatus);
-                        }else {
+                        } else {
                             System.out.println("Please enter a correct index.");
                         }
                     }
-//
-
-//                    int i = sc.nextInt();
-//                    Subject subjectInList = null;
-//                    if(student.getSubjectAtIndex(i) == null){
-//                        System.out.println("Please add a Subject first");
-//                    }
-//                    else{
-//                         subjectInList = student.getSubjectAtIndex(i);
-//                    }
-//
-//
-
                     break;
 
                 case 6:
@@ -146,70 +132,11 @@ public class AttendanceTrackerApplication {
                     System.out.println("Number out of Range.");
                     break;
             }
-        }while (keepRunning);
+        } while (keepRunning);
 
 
-//        Scanner sc = new Scanner(System.in);
-
-//        Subject subject1 = new Subject("Java");
-//        Subject subject2 = new Subject("Java");
-//        System.out.println(subject1.equals(subject2));
-//
-//        int i = sc.nextInt();
-//        switch (i){
-//            case 1:
-//                System.out.println("Add subject");
-//                student.addSubject(subject);
-//                break;
-//
-//            case 2:
-//                System.out.println("View Subject");
-//                student.viewSubject();
-//                break;
-//
-//            case 3:
-//                System.out.println("Create TimetableEntry");
-//                student.createTimetable(timetableEntry);
-//                break;
-//
-//            case 4:
-//                System.out.println("Add Attendance");
-//                student.addAttendanceRecord(attendanceStatus);
-//                break;
-//
-//            case 5:
-//                System.out.println("View Attendance");
-//                student.viewAttendanceReport();
-//                break;
-//
-//            default:
-//                System.out.println("Exit");
-//                break;
-
-
-
-//        TimetableEntry timetableEntry = new TimetableEntry(time, dayOfWeek, subject);
-//        student.createTimetable(timetableEntry);
-//
-//        AttendanceStatus attendanceStatus = new AttendanceStatus(date, subject, present);
-//        student.addAttendanceRecord(attendanceStatus);
-
-//        System.out.println("Enter Time schedule of Lecture");
-//        String time = sc.next();
-//
-//        System.out.println("Enter the particular Day of Lecture");
-//        String dayOfWeek = sc.next();
-//
-//        System.out.println("Enter Date");
-//        String date = sc.next();
-//
-//        System.out.println("Enter true for present and false for absent");
-//        boolean present = sc.nextBoolean();
-//       }
     }
 }
-
-
 /*
 1. addSubject() bug
 2. Add Attendance when no subjects exist
